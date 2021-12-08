548f9922aea126541fc290f0dc358e19f739638c
viv-codes <79525734+viv-codes@users.noreply.github.com>
2021-12-08 11:39:05 -0500

    Cleaned my slate

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-APP = Flask(__name__)
-
-from .app import APP as app_blueprint
-APP.register_blueprint(app_blueprint)
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-APP's primary entry point
-"""
-
-from app import APP
-
-if __name__ == "__main__":
-    APP.run(host=APP.config["IP"], port=int(APP.config["PORT"]))
